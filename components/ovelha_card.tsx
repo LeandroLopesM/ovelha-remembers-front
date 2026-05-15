530f3d8b68a59778c2c09f2ad7ec3242cedb50d6
Leandro Lopes <leandrolopesm@gmail.com>
2026-05-15 17:36:50 -0300

    iniciado o ovelha adder

---
+++ components/ovelha_card.tsx
@@ -0,0 +1,38 @@
+import { Ovelha } from '@/app/util/types';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { ReactNode } from 'react';
+import { StyleSheet, Text, View } from "react-native";
+
+type OvelhaCardProps = {
+    info: Ovelha,
+    children?: string | ReactNode
+}
+export default function OvelhaCard({info}: OvelhaCardProps) {
+    return (
+        <View style={style.container}>
+            <Text style={style.name}>{info.name}</Text>
+            <AntDesign name="caret-right" size={15} color="black" style={style.proceed}/>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '5%',
+        flex: 1,
+        flexDirection: 'row',
+        padding: 4
+    },
+    name: {
+        fontSize: 20,
+        zIndex: 1,
+    },
+    proceed: {
+        alignSelf: 'flex-end',
+        zIndex: 2,
+        marginRight: 0,
+        margin: 'auto',
+
+    }
+})
--- components/ovelhas_list.tsx
+++ components/ovelhas_list.tsx
@@ -2,8 +2,10 @@ import { Ovelha, OvelhaList } from "@/app/util/types";
 import { style } from "@/conf";
 import statelyFetch from "@/scripts/statelyFetch";
 import { TESTING_OLIST } from "@/scripts/testing_data";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import OvelhaCard from "./ovelha_card";
 import Failure from "./utils/failure";
 
 export default function OvelhasList() {
@@ -29,8 +31,31 @@ export default function OvelhasList() {
 
     useEffect(() => { getDalvas(); }, []);
 
+    const [ovelhaManagerVisible, setOvelhaManagerVisible] = useState(false)
+    const [OMName, setOMName] = useState<string>('')
+    const [OMNasc, setOMNasc] = useState<Date>(new Date(0))
+    const [OMGen, setOMGen] = useState<string>('')
+
     return (
         <View style={style.subContainer}>
+            {/* //TODO: Finish modal para adicionar ovelha!! */}
+            <Modal
+                style={OMStyle.popup}
+                visible={ovelhaManagerVisible}
+                transparent={true}
+                onRequestClose={() => {
+                    setOvelhaManagerVisible(!ovelhaManagerVisible);
+                }}>
+                <TextInput onChangeText={setOMName} placeholder="Nome"></TextInput>
+                <TextInput onChangeText={t => setOMNasc(new Date(t))} placeholder="Nascimento"></TextInput>
+                <TextInput onChangeText={setOMGen} placeholder="Gênero"></TextInput>
+                <TouchableOpacity onPress={(_) => setOvelhaManagerVisible(false)}>Pronto</TouchableOpacity>
+            </Modal>
+
+            <TouchableOpacity style={oStyle.subSubContainer} onPress={(_) => setOvelhaManagerVisible(true)}>
+                <Text style={[oStyle.title, style.dailyTitle]}>Ovelhas</Text>
+                <FontAwesome5 name="plus" size={24} color="black" style={oStyle.add} />
+            </TouchableOpacity>
             {
             progress === 'wait' ? (
                 <ActivityIndicator></ActivityIndicator>
@@ -40,7 +65,7 @@ export default function OvelhasList() {
                     data={data as Ovelha[]}
                     renderItem={({item}) => (
                         // <Ionicons name="checkmark-circle" size={32} />
-                        <Text>{item.name}</Text>
+                        <OvelhaCard info={item}></OvelhaCard>
                     )}
                     ListEmptyComponent={() => (
                         <Text>Sem ovelhas!</Text>
@@ -52,3 +77,40 @@ export default function OvelhasList() {
         </View>
     );
 }
+
+const oStyle = StyleSheet.create({
+    add: {
+        alignSelf: 'flex-end',
+        marginRight: '5%',
+        margin: 'auto',
+    },
+    title: {
+        margin: 'auto',
+        left: '16%',
+    },
+    subSubContainer: {
+        width: '100%',
+        height: '15%',
+        // flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})
+
+const OMStyle = StyleSheet.create({
+    popup: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+        width: 0,
+        height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+})
